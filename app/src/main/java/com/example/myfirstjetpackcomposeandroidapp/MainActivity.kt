82a39e06e19ed78b86ea18be1321ac3b0144e7c0
Nguyễn Công Tuấn <147248673+TOIsMeMan@users.noreply.github.com>
2024-09-24 15:11:04 +0700

    before update ui

--- app/src/main/java/com/example/myfirstjetpackcomposeandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstjetpackcomposeandroidapp/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.myfirstjetpackcomposeandroidapp
 
 import android.os.Bundle
-import android.text.Layout
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,131 +23,210 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierInfo
-import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.myfirstjetpackcomposeandroidapp.ui.theme.MyFirstJetpackComposeAndroidAppTheme
-import kotlin.reflect.KProperty
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import com.example.myfirstjetpackcomposeandroidapp.ui.theme.MyFirstJetpackComposeAndroidAppTheme
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : ComponentActivity() {
+    private val ESP8266_URL = "http://10.10.27.246"
+    private val TAG: String = "HTTP_Response"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyFirstJetpackComposeAndroidAppTheme {
-                Surface (
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color(red = 4, green = 41, blue = 64),
+                                    Color(red = 0, green = 92, blue = 83)
+                                )
+                            )
+                        )
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 16.dp, top = 50.dp, end = 16.dp, bottom = 50.dp),
-                        verticalArrangement = Arrangement.spacedBy(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
+                    Surface (
+                        modifier = Modifier.fillMaxSize(),
+                        color = Color.Transparent
                     ) {
-                        var lightIsOn by remember { mutableStateOf(false) }
-                        var textOfLightButton by remember { mutableStateOf("Bat den") }
-                        Button(
-                            onClick = {
-                                lightIsOn = !lightIsOn
-                                textOfLightButton = if (lightIsOn) "Tat den" else "Bat den"
-                                      },
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = if (lightIsOn) Color.Green else Color.Gray
-                            ),
+                        Column(
                             modifier = Modifier
                                 .fillMaxSize()
-                                .weight(1f),
-                            shape = RoundedCornerShape(30.dp)
+                                .padding(start = 16.dp, top = 50.dp, end = 16.dp, bottom = 50.dp),
+                            verticalArrangement = Arrangement.spacedBy(16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
                         ) {
-                            Text(
-                                text = "${textOfLightButton}",
-                                color = Color.White,
-                                fontSize = 30.sp
-                            )
-                        }
-                        var fanIsOn by remember { mutableStateOf(false) }
-                        var textOfFanButton by remember { mutableStateOf("Bat quat") }
-                        Button(
-                            onClick = {
-                                fanIsOn = !fanIsOn
-                                textOfFanButton = if (fanIsOn) "Tat quat" else "Bat quat"
-                                      },
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = if (fanIsOn) Color.Green else Color.Gray
-                            ),
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .weight(1f),
-                            shape = RoundedCornerShape(30.dp)
-                        ) {
-                            Text(
-                                text = "${textOfFanButton}",
-                                color = Color.White,
-                                fontSize = 30.sp
-                            )
-                        }
-                        var doorIsOpen by remember { mutableStateOf(false) }
-                        var textOfDoorButton by remember { mutableStateOf("Mo cua") }
-                        Button(
-                            onClick = {
-                                doorIsOpen = !doorIsOpen
-                                textOfDoorButton = if (doorIsOpen) "Dong cua" else "Mo cua"
-                                      },
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = if (doorIsOpen) Color.Green else Color.Gray
-                            ),
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .weight(1f),
-                            shape = RoundedCornerShape(30.dp)
-                        ) {
-                            Text(
-                                text = "${textOfDoorButton}",
-                                color = Color.White,
-                                fontSize = 30.sp
-                            )
-                        }
-                        var micIsOn by remember { mutableStateOf(false) }
-                        var textOfMicButton by remember { mutableStateOf("Mic") }
-                        Button(
-                            onClick = {
-                                micIsOn = !micIsOn
-                                textOfMicButton = if (micIsOn) "Noi Gi Di" else "Mic"
-                                      },
-                            modifier = Modifier
-                                .width(90.dp)
-                                .height(90.dp)
-                                .align(Alignment.End),
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = if (micIsOn) Color.Green else Color.Gray
-                            ),
-                            shape = RoundedCornerShape(30.dp)
-                        ) {
-                            Text(
-                                text = "${textOfMicButton}",
-                                color = Color.White,
-                                fontSize = 15.sp,
-                                textAlign = TextAlign.Center
-                            )
+                            var lightIsOn by remember { mutableStateOf(false) }
+                            var textOfLightButton by remember { mutableStateOf("Bật đèn") }
+                            Button(
+                                onClick = {
+                                    lightIsOn = !lightIsOn
+                                    textOfLightButton = if (lightIsOn) "Tắt đèn" else "Bật đèn"
+                                    if (lightIsOn) {
+                                        sendRequest("/light/on")
+                                    } else {
+                                        sendRequest("/light/off")
+                                    }
+                                },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = if (lightIsOn) Color(red = 43, green = 140, blue = 68) else Color(red = 143, green = 193, blue = 181)
+                                ),
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .weight(1f),
+                                shape = RoundedCornerShape(30.dp)
+                            ) {
+                                Text(
+                                    text = "${textOfLightButton}",
+                                    color = Color.White,
+                                    fontSize = 30.sp
+                                )
+                            }
+                            var fanIsOn by remember { mutableStateOf(false) }
+                            var textOfFanButton by remember { mutableStateOf("Bật quạt") }
+                            Button(
+                                onClick = {
+                                    fanIsOn = !fanIsOn
+                                    textOfFanButton = if (fanIsOn) "Tắt quạt" else "Bật quạt"
+                                    if (fanIsOn) {
+                                        sendRequest("/fan/on")
+                                    } else {
+                                        sendRequest("/fan/off")
+                                    }
+                                },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = if (fanIsOn) Color(red = 43, green = 140, blue = 68) else Color(red = 64, green = 64, blue = 64)
+                                ),
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .weight(1f),
+                                shape = RoundedCornerShape(30.dp)
+                            ) {
+                                Text(
+                                    text = "${textOfFanButton}",
+                                    color = Color.White,
+                                    fontSize = 30.sp
+                                )
+                            }
+                            var doorIsOpen by remember { mutableStateOf(false) }
+                            var textOfDoorButton by remember { mutableStateOf("Mở cửa") }
+                            Button(
+                                onClick = {
+                                    doorIsOpen = !doorIsOpen
+                                    textOfDoorButton = if (doorIsOpen) "Đóng cửa" else "Mở cửa"
+                                    if (doorIsOpen) {
+                                        sendRequest("/door/open")
+                                    } else {
+                                        sendRequest("/door/close")
+                                    }
+                                },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = if (doorIsOpen) Color(red = 43, green = 140, blue = 68) else Color(red = 64, green = 64, blue = 64)
+                                ),
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .weight(1f),
+                                shape = RoundedCornerShape(30.dp)
+                            ) {
+                                Text(
+                                    text = "${textOfDoorButton}",
+                                    color = Color.White,
+                                    fontSize = 30.sp
+                                )
+                            }
+                            Row (
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(90.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Button(
+                                    onClick = { /*TODO*/ },
+                                    modifier = Modifier
+                                        .fillMaxHeight()
+                                        .weight(1f),
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = Color.Transparent
+                                    )
+                                ) {
+                                    Text(
+                                        text = "Nhà Thông Minh",
+                                        color = Color.White,
+                                        textAlign = TextAlign.Center,
+                                        fontSize = 30.sp
+                                    )
+                                }
+
+                                var micIsOn by remember { mutableStateOf(false) }
+                                var textOfMicButton by remember { mutableStateOf("Mic") }
+                                Button(
+                                    onClick = {
+                                        micIsOn = !micIsOn
+                                        textOfMicButton = if (micIsOn) "Nói gì đi" else "Mic"
+                                    },
+                                    modifier = Modifier
+                                        .width(90.dp)
+                                        .fillMaxHeight()
+                                        .padding(end = 0.dp),
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = if (micIsOn) Color(red = 43, green = 140, blue = 68) else Color(red = 64, green = 64, blue = 64)
+                                    ),
+                                    shape = RoundedCornerShape(30.dp)
+                                ) {
+                                    Text(
+                                        text = "${textOfMicButton}",
+                                        color = Color.White,
+                                        fontSize = 20.sp,
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            }
                         }
                     }
                 }
+
             }
         }
+    }
+    // Hàm gửi yêu cầu HTTP GET đến ESP8266
+    private fun sendRequest(endpoint: String) {
+        Thread {
+            try {
+                // Tạo URL kết nối đến ESP8266
+                val url = URL(ESP8266_URL + endpoint)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+
+                // Kiểm tra mã phản hồi HTTP từ server
+                val responseCode = connection.responseCode
+                Log.d(TAG, "Response Code: $responseCode")
+
+                if (responseCode == 200) {  // Nếu mã phản hồi là 200 OK
+                    val response = connection.inputStream.bufferedReader().use { it.readText() }
+                    Log.d(TAG, "Response: $response")
+                } else {
+                    // Xử lý khi nhận phản hồi lỗi
+                    Log.e(TAG, "Error Response Code: $responseCode")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Log.e(TAG, "Exception: ${e.message}")
+            }
+        }.start()
     }
 }
 
